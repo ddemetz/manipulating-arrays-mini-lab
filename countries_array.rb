663ca8ea48a8f0051cc07470b8489b29620df9c6
Dominique Demetz <ddemetz123@icloud.com>
2017-06-21 18:39:49 +0000

    done

--- countries_array.rb
+++ countries_array.rb
@@ -3,10 +3,10 @@ four_letter_countries=["Chad", "Cuba", "Greenland", "Iraq", "Mali", "Oman", "Ind
  four_letter_countries=four_letter_countries.push("Fiji")
  four_letter_countries=four_letter_countries.insert(4, "Iran")
  four_letter_countries.delete ("India")
- four_letter_countries.reverse
- four_letter_countries[2]= "Togo"
+ four_letter_countries=four_letter_countries.reverse
+ four_letter_countries[5]= "Togo"
  four_letter_countries=four_letter_countries.push("Laos")
- four_letter_countries.reverse
+ four_letter_countries=four_letter_countries.reverse
  four_letter_countries=four_letter_countries.unshift("Peru")
  
- p four_letter_countries
+ puts four_letter_countries
